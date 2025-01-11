34147acab253f66993dd17d08d1dc68d6475ddfd
zouyuyang <zouyuyang@buaa.edu.cn>
2025-01-11 19:58:02 +0800

    change arty100t to nexys4ddr board

--- fpga/src/main/scala/arty100t/HarnessBinders.scala
+++ fpga/src/main/scala/arty100t/HarnessBinders.scala
@@ -25,19 +25,19 @@ class WithArty100TUARTTSI extends HarnessBinder({
     val harnessIO = IO(new UARTPortIO(port.io.uartParams)).suggestName("uart_tsi")
     harnessIO <> port.io.uart
     val packagePinsWithPackageIOs = Seq(
-      ("A9" , IOPin(harnessIO.rxd)),
-      ("D10", IOPin(harnessIO.txd)))
+      ("C4" , IOPin(harnessIO.rxd)),
+      ("D4", IOPin(harnessIO.txd)))
     packagePinsWithPackageIOs foreach { case (pin, io) => {
       ath.xdc.addPackagePin(io, pin)
       ath.xdc.addIOStandard(io, "LVCMOS33")
       ath.xdc.addIOB(io)
     } }
 
-    ath.other_leds(1) := port.io.dropped
-    ath.other_leds(9) := port.io.tsi2tl_state(0)
-    ath.other_leds(10) := port.io.tsi2tl_state(1)
-    ath.other_leds(11) := port.io.tsi2tl_state(2)
-    ath.other_leds(12) := port.io.tsi2tl_state(3)
+    // ath.other_leds(1) := port.io.dropped
+    // ath.other_leds(9) := port.io.tsi2tl_state(0)
+    // ath.other_leds(10) := port.io.tsi2tl_state(1)
+    // ath.other_leds(11) := port.io.tsi2tl_state(2)
+    // ath.other_leds(12) := port.io.tsi2tl_state(3)
   }
 })
 
@@ -53,6 +53,7 @@ class WithArty100TDDRTL extends HarnessBinder({
 })
 
 // Uses PMOD JA/JB
+// Not initialized, don't care
 class WithArty100TSerialTLToGPIO extends HarnessBinder({
   case (th: HasHarnessInstantiators, port: SerialTLPort, chipId: Int) => {
     val artyTh = th.asInstanceOf[LazyRawModuleImp].wrapper.asInstanceOf[Arty100THarness]
@@ -121,7 +122,7 @@ class WithArty100TUART(rxdPin: String = "A9", txdPin: String = "D10") extends Ha
 })
 
 // Maps the UART device to PMOD JD pins 3/7
-class WithArty100TPMODUART extends WithArty100TUART("G2", "F3")
+class WithArty100TPMODUART extends WithArty100TUART("J4", "E6")
 
 class WithArty100TJTAG extends HarnessBinder({
   case (th: HasHarnessInstantiators, port: JTAGPort, chipId: Int) => {
@@ -133,10 +134,10 @@ class WithArty100TJTAG extends HarnessBinder({
     ath.sdc.addGroup(clocks = Seq("JTCK"))
     ath.xdc.clockDedicatedRouteFalse(IOPin(harnessIO.TCK))
     val packagePinsWithPackageIOs = Seq(
-      ("F4", IOPin(harnessIO.TCK)),
-      ("D2", IOPin(harnessIO.TMS)),
-      ("E2", IOPin(harnessIO.TDI)),
-      ("D4", IOPin(harnessIO.TDO))
+      ("H4", IOPin(harnessIO.TCK)),
+      ("H1", IOPin(harnessIO.TMS)),
+      ("G1", IOPin(harnessIO.TDI)),
+      ("G3", IOPin(harnessIO.TDO))
     )
     packagePinsWithPackageIOs foreach { case (pin, io) => {
       ath.xdc.addPackagePin(io, pin)
